1e69d2fdcdffbfa9f7394bec49ff02dd7c56cae9
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-03-15 16:38:04 +0000

    Imported extra flask modules, PyMongo and ObjectId. Set up get_tasks()

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import os
-from flask import Flask
-from flask_pymongo import pymongo
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'level-up'
 app.config["MONGO_URI"] = 'mongodb://admin:Strat3gic@ds127115.mlab.com:27115/level-up'
 
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Sup, name change successful, 2nd test change"
+@app.route('/get_tracks')
+def get_tasks():
+    return render_template("tracks.html",
+    tracks=mongo.db.tracks.find())
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
