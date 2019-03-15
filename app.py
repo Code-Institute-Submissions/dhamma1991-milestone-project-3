d123433317e7ed05709790b9176c66a8b314f3e9
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-03-15 16:29:30 +0000

    Test change to test rename of heroku app

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
 from flask import Flask
+from flask_pymongo import pymongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'level-up'
+app.config["MONGO_URI"] = 'mongodb://admin:Strat3gic@ds127115.mlab.com:27115/level-up'
+
 @app.route('/')
 def hello():
-    return "Sup, name change successful"
+    return "Sup, name change successful, 2nd test change"
     
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
