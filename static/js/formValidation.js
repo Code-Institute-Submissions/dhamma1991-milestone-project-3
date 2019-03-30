4a59a50e2a3208dbc635a7c5ee50140c17db8b7e
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-03-30 11:41:08 +0000

    Set up add-track form validation to disable default, using Materialize to display custom error message for year

--- static/js/formValidation.js
+++ static/js/formValidation.js
@@ -1,9 +1,15 @@
-var email = document.getElementById("mail");
+// There are many ways to pick a DOM node; here we get the form itself and the email
+// input box, as well as the span element into which we will place the error message.
 
-email.addEventListener("input", function (event) {
-  if (email.validity.typeMismatch) {
-    email.setCustomValidity("I expect an e-mail, darling!");
-  } else {
-    email.setCustomValidity("");
+var form  = document.getElementsByTagName('form')[0];
+var email = document.getElementById('year');
+var error = document.querySelector('.error');
+
+form.addEventListener("submit", function (event) {
+  // Each time the user tries to send the data, we check
+  // if the email field is valid.
+  if (!year.validity.valid) {
+    // And we prevent the form from being sent by canceling the event
+    event.preventDefault();
   }
-});
+}, false);
