0341d40923796cc5eecb4a2bf4d486044b06b1c8
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-03-30 13:25:58 +0000

    Added jquery to add active class to form labels if not valid when form submitted

--- static/js/formValidation.js
+++ static/js/formValidation.js
@@ -11,8 +11,37 @@ var form  = document.getElementsByTagName('form')[0],
 form.addEventListener("submit", function (event) {
   // Each time the user tries to send the data, we check
   // if the email field is valid.
-  if (!track_title.validity.valid || !artist.validity.valid || !youtube_link.validity.valid || !year.validity.valid)  {
-    // And we prevent the form from being sent by canceling the event
-    event.preventDefault();
-  }
+//   if (!track_title.validity.valid || !artist.validity.valid || !youtube_link.validity.valid || !year.validity.valid)  {
+    if (!track_title.validity.valid) {
+        $("#track_title").removeClass("valid");
+        $("#track_title").addClass("invalid");
+        $("#track_title_label").addClass("active");
+        $("#track_title").prop("aria-invalid", "true");
+        // And we prevent the form from being sent by canceling the event
+        event.preventDefault();
+    }
+    if (!artist.validity.valid) {
+        $("#artist").removeClass("valid");
+        $("#artist").addClass("invalid");
+        $("#artist_label").addClass("active");
+        $("#artist").prop("aria-invalid", "true");
+        // And we prevent the form from being sent by canceling the event
+        event.preventDefault();
+    }
+    if (!youtube_link.validity.valid) {
+        $("#youtube_link").removeClass("valid");
+        $("#youtube_link").addClass("invalid");
+        $("#youtube_link_label").addClass("active");
+        $("#youtube_link").prop("aria-invalid", "true");
+        // And we prevent the form from being sent by canceling the event
+        event.preventDefault();
+    }
+    if (!year.validity.valid) {
+        $("#year").removeClass("valid");
+        $("#year").addClass("invalid");
+        $("#year_label").addClass("active");
+        $("#year").prop("aria-invalid", "true");
+        // And we prevent the form from being sent by canceling the event
+        event.preventDefault();
+    }
 }, false);
