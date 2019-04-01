45e2532d73416658cdf0fbdb5b06de0a429fc6ff
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-04-01 11:45:31 +0000

    Modified comments

--- static/js/formValidation.js
+++ static/js/formValidation.js
@@ -1,6 +1,3 @@
-// There are many ways to pick a DOM node; here we get the form itself and the email
-// input box, as well as the span element into which we will place the error message.
-
 var form  = document.getElementsByTagName('form')[0],
     track_title = document.getElementById('track_title'),
     artist = document.getElementById('artist'),
